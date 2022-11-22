f31403c45b6c3c71af4d965139231dc7626117e0
Greg.Study <greg0917@naver.com>
2022-11-23 00:28:44 +0900

    세팅

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "첫번 째 블로그입니다!",
+        "excerpt":"[공지사항] 지킬블로그 신규 업데이트 안내 드립니다. 공지사항입니다. 공지사항 순서 1 공지사항 순서 2 공지사항 순서 3 테스트 수정! 버튼입니다 This is a Test hello, world! Okay Let’s go wow 와우~ Hello world, this is my first Jekyll blog post. I hope you like it! I hope you like it! I...","categories": ["coding"],
+        "tags": ["python","blog","jekyll"],
+        "url": "/coding/first-posting/",
+        "teaser": null
+      },{
+        "title": "Jupyter 노트북 변환 예제",
+        "excerpt":"(Seaborn 고유의) 통계 기반의 그래프 seaborn 라이브러리가 매력적인 이유는 바로 통계 차트 입니다. 이번 실습에서는 seaborn의 다양한 통계 차트 중 대표적인 차트 몇 개를 뽑아서 다뤄볼 예정입니다. 더 많은 통계 차트를 경험해보고 싶으신 분은 공식 도큐먼트에서 확인하실 수 있습니다. 모듈 Import from IPython.display import Image import numpy as np import...","categories": ["jupyter"],
+        "tags": ["python","blog","jupyter"],
+        "url": "/jupyter/%EC%83%98%ED%94%8C-JupyterNotebook/",
+        "teaser": null
+      }]
