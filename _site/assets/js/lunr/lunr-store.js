7056740802a83e681eb6e6d2a79a9c640045dd39
Greg.Study <greg0917@naver.com>
2022-11-30 23:51:27 +0900

    정적 팩토리 메서드

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
         "title": "22.11.4",
         "excerpt":"equals &amp; hashcode Object 클래스의 메소드이다. 모든 클래스가 Object를 상속받기 때문에 어떤 객체라도 Object의 메소드인 equals와 hashcode를 사용할 수 있다. equals // Object의 equals() public boolean equals(Object obj) { return (this == obj); } equals()는 오로지 참조값(객체의 주소값)이 같은지 즉, 동일 객체인지 확인하는 기능이다. equals 메소드는 두 객체를 비교해서 논리적으로...","categories": ["TIL"],
-        "tags": ["java","디자인패턴"],
+        "tags": ["java","디자인패턴","이펙티브자바"],
         "url": "/til/",
         "teaser": null
       }]
